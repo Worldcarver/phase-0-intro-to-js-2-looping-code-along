0a78269be1f389c74dc8e57ffecb25e6b465fd1d
ian <ian.murphy92@gmail.com>
2022-05-19 19:04:06 -0400

    commit tree c4103dd800b7c123a79da64abd606891b70ed726

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const writeCards = (names, event) => {
+    let messages = [];
+    for (let i=0; i <3; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages
+}
+
+const countDown = () =>{
+    let number = 10
+    while (number > -1) {
+        console.log(number--)
+    }
+}
+console.log(number)
